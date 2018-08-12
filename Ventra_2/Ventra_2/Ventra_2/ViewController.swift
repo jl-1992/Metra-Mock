72ed49c0141c6147320b2ce744094593b6132af8
Joseph Ludy <joseph.earl.ludy@gmail.com>
2018-08-11 20:29:31 -0500

    cloud animation

--- Ventra_2/Ventra_2/Ventra_2/ViewController.swift
+++ Ventra_2/Ventra_2/Ventra_2/ViewController.swift
@@ -10,11 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var clouds: UIImageView!
-
+    
     var timer = Timer()
     let formatter: DateFormatter = {
         let tmpFormatter = DateFormatter()
@@ -32,23 +30,24 @@ class ViewController: UIViewController {
     
     func animateClouds() {
         let backgroundImage = UIImage(named:"Clouds")!
-        let amountToKeepImageSquare = backgroundImage.size.height - self.view.frame.size.height
+        let amountToKeepImageSquare = backgroundImage.size.height - self.clouds.frame.size.height
         
         // UIImageView 1
         let backgroundImageView1 = UIImageView(image: backgroundImage)
-       // backgroundImageView1.frame = clouds.frame
-        backgroundImageView1.frame = CGRect(x: self.view.frame.origin.x, y: self.view.frame.origin.y, width: backgroundImage.size.width - amountToKeepImageSquare, height: self.view.frame.size.height)
+        backgroundImageView1.contentMode = UIViewContentMode.scaleAspectFit
+        backgroundImageView1.frame = CGRect(x: self.clouds.frame.origin.x, y: self.clouds.frame.origin.y, width: backgroundImage.size.width - amountToKeepImageSquare, height: self.clouds.frame.size.height)
         self.view.addSubview(backgroundImageView1)
         
         // UIImageView 2
         let backgroundImageView2 = UIImageView(image: backgroundImage)
-       // backgroundImageView2.frame = clouds.frame
-        backgroundImageView2.frame = CGRect(x: backgroundImageView1.frame.size.width, y: self.view.frame.origin.y, width: backgroundImage.size.width - amountToKeepImageSquare, height: self.view.frame.height)
+        backgroundImageView2.contentMode = UIViewContentMode.scaleAspectFit
+        backgroundImageView2.frame = CGRect(x: backgroundImageView1.frame.size.width, y: self.clouds.frame.origin.y, width: backgroundImage.size.width - amountToKeepImageSquare, height: self.clouds.frame.height)
         self.view.addSubview(backgroundImageView2)
         
-        UIView.animate(withDuration: 6.0, delay: 0, options: [.repeat, .curveLinear], animations: { backgroundImageView1.frame = backgroundImageView1.frame.offsetBy(dx: -1 * backgroundImageView1.frame.size.width, dy: 0.0)
+        UIView.animate(withDuration: 20.0, delay: 0, options: [.repeat, .curveLinear], animations: { backgroundImageView1.frame = backgroundImageView1.frame.offsetBy(dx: -1 * backgroundImageView1.frame.size.width, dy: 0.0)
             backgroundImageView2.frame = backgroundImageView2.frame.offsetBy(dx: -1 * backgroundImageView2.frame.size.width, dy: 0.0) }, completion: nil)
     }
+
     
     func setTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
