a290cb569c1b35847a8894de14fe01159ce1ed07
Joseph Ludy <joseph.earl.ludy@gmail.com>
2018-08-23 15:27:43 -0500

    added people

--- Ventra_2/Ventra_2/Ventra_2/ViewController.swift
+++ Ventra_2/Ventra_2/Ventra_2/ViewController.swift
@@ -7,8 +7,8 @@
 //
 
 // TO-DO:
-// 1. Add in moving people
-// 2. Add in on-click event
+// 1. Add in on-click event
+// 2. Check timing of entire view
 // 3. General cleanup of layout
 
 import UIKit
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var trainBack: UIImageView!
     @IBOutlet weak var station: UIImageView!
     @IBOutlet weak var skyline: UIImageView!
+    @IBOutlet weak var people: UIImageView!
     
     var timer = Timer()
     let formatter: DateFormatter = {
@@ -36,6 +37,7 @@ class ViewController: UIViewController {
         animateClouds()
         animateFrontTrain()
         animateBackTrain()
+        animatePeople()
     }
     
     func animateClouds() {
@@ -104,7 +106,100 @@ class ViewController: UIViewController {
             backgroundImageView1.frame = backgroundImageView1.frame.offsetBy(dx: 1 * backgroundImageView1.frame.size.width, dy: 0.0)
             backgroundImageView2.frame = backgroundImageView2.frame.offsetBy(dx: 1 * backgroundImageView2.frame.size.width, dy: 0.0)}, completion: nil)
     }
+    
+    // Define people image values
+    
+    
+    
+    func animatePeople() {
+        // First two people who enter from left
+        let image1 = UIImage(named:"People_1")!
+        
+        let people1 = UIImageView(image: image1)
+        people1.contentMode = UIViewContentMode.scaleAspectFit
+        people1.frame = CGRect(x: self.people.frame.origin.x, y: self.people.frame.origin.y, width: image1.size.width, height: self.people.frame.size.height)
+        self.view.addSubview(people1)
 
+        self.view.insertSubview(people1, belowSubview: station)
+        
+        UIView.animate(withDuration: 5.0, delay: 0, options: [.curveLinear], animations: {
+            people1.frame = people1.frame.offsetBy(dx: 4 * people1.frame.size.width, dy: 0.0)}, completion: nil)
+        
+        // Person 1 (from left)
+        let image2 = UIImage(named:"Person")!
+        
+        let person1 = UIImageView(image: image2)
+        person1.contentMode = UIViewContentMode.scaleAspectFit
+        person1.frame = CGRect(x: self.people.frame.origin.x - 45, y: self.people.frame.origin.y, width: image2.size.width, height: self.people.frame.size.height)
+        self.view.addSubview(person1)
+        
+        self.view.insertSubview(person1, belowSubview: station)
+        
+        UIView.animate(withDuration: 4.0, delay: 4.0, options: [.curveLinear], animations: {
+            person1.frame = person1.frame.offsetBy(dx: self.people.frame.maxX, dy: 0.0)
+        }, completion: { finished in
+            if finished {
+                UIView.animate(withDuration: 4.0, delay: 0, options: [.curveLinear], animations: {
+                    person1.frame = person1.frame.offsetBy(dx: -self.people.frame.maxX - 30, dy: 0.0)
+                }, completion: nil)
+            }
+        })
+        
+        // First two people who enter from right
+        let image3 = UIImage(named:"People_3")!
+        
+        let people3 = UIImageView(image: image3)
+        people3.contentMode = UIViewContentMode.scaleAspectFit
+        people3.frame = CGRect(x: self.people.frame.maxX, y: self.people.frame.origin.y, width: image1.size.width, height: self.people.frame.size.height)
+        self.view.addSubview(people3)
+        
+        self.view.insertSubview(people3, belowSubview: station)
+        
+        UIView.animate(withDuration: 5.0, delay: 4.5, options: [.curveLinear], animations: {
+            people3.frame = people3.frame.offsetBy(dx: -self.people.frame.size.width - people3.frame.size.width, dy: 0.0)}, completion: nil)
+
+        // Person 2 (from left)
+        let person2 = UIImageView(image: image2)
+        person2.contentMode = UIViewContentMode.scaleAspectFit
+        person2.frame = CGRect(x: self.people.frame.origin.x - 45, y: self.people.frame.origin.y, width: image2.size.width, height: self.people.frame.size.height)
+        self.view.addSubview(person2)
+        
+        self.view.insertSubview(person2, belowSubview: station)
+        
+        UIView.animate(withDuration: 4.0, delay: 5.0, options: [.curveLinear], animations: {
+            person2.frame = person2.frame.offsetBy(dx: self.people.frame.maxX + 20, dy: 0.0)
+        }, completion: nil)
+        
+        // Second two people from right
+        let people4 = UIImageView(image: image1)
+        people4.contentMode = UIViewContentMode.scaleAspectFit
+        people4.frame = CGRect(x: self.people.frame.maxX, y: self.people.frame.origin.y, width: image1.size.width, height: self.people.frame.size.height)
+        self.view.addSubview(people4)
+        
+        self.view.insertSubview(people4, belowSubview: station)
+        
+        // Last two people from left that end abruptly
+        let people5 = UIImageView(image: image1)
+        people5.contentMode = UIViewContentMode.scaleAspectFit
+        people5.frame = CGRect(x: self.people.frame.origin.x - people5.frame.size.width, y: self.people.frame.origin.y, width: image1.size.width, height: self.people.frame.size.height)
+        self.view.addSubview(people5)
+        
+        self.view.insertSubview(people5, belowSubview: station)
+        
+        // Animate people4
+        UIView.animate(withDuration: 7.0, delay: 8.5, options: [.curveLinear], animations: {
+            people4.frame = people4.frame.offsetBy(dx: -self.people.frame.size.width, dy: 0.0)}, completion: { finished in
+                if finished {
+                    people5.isHidden = true
+                    people4.isHidden = true
+                    self.animatePeople()
+                }
+        })
+        
+        // Animate people5
+        UIView.animate(withDuration: 7.0, delay: 9.0, options: [.curveLinear], animations: {
+            people5.frame = people5.frame.offsetBy(dx: 2 * people5.frame.size.width, dy: 0.0)}, completion: nil)
+    }
     
     func setTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
